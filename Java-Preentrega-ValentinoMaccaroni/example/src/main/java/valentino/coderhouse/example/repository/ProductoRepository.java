09a97fd9ee357abc1562cd4bbe106bf3546b1f05
valenmacca <167814525+valenmacca@users.noreply.github.com>
2024-10-18 15:27:44 -0300

    Delete Java-Preentrega-ValentinoMaccaroni/example directory

--- Java-Preentrega-ValentinoMaccaroni/example/src/main/java/valentino/coderhouse/example/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package tomi.coderhouse.example.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tomi.coderhouse.example.entity.Product;
-
-public interface ProductoRepository extends JpaRepository<Product,Long>{ }
